b8c23d8a16f862c072cebda1d704e046964d4648
James Goatcher <webdevil81@gmail.com>
2020-11-11 16:32:00 +0000

    Function for creating and adding book cards to page

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let myLibrary = [];
-const libraryContainer = document.querySelector('.library');
-
 /***************************************************
  * Modal button popup form *
  ***************************************************/
@@ -27,8 +24,10 @@ window.onclick = function(e){
  * Modal form values to objects and pushed to array *
  ****************************************************/
 
+let myLibrary = [];
+
 function addBook(e) {
-  e.preventDefault();
+  e.preventDefault(); //Prevents page refresh from form submit
   let book = {
       name: document.querySelector('#name').value,
       author: document.querySelector('#author').value,
@@ -36,15 +35,54 @@ function addBook(e) {
       read: document.querySelector('#read').value
   };   
   myLibrary.push(book);
+  //Clears input dields
   document.querySelector('#name').value = '';
   document.querySelector('#author').value = '';
   document.querySelector('#number').value = '';
   document.querySelector('#read').value = '';
   modal.style.display = "none";
+  addToLibrary();
 }
 
 /****************************************************
  * Create library cards *
  ****************************************************/
 
- 
+const library = document.querySelector('.library');
+
+function addToLibrary() {
+  removeAllChildNodes(library);  
+  myLibrary.forEach(book => {
+    
+    const libraryCard = document.createElement('div');
+    libraryCard.classList.add('card');
+    
+    const name = document.createElement('p');
+    name.textContent = book.name;
+    name.classList.add('bookCardElement');
+    const author = document.createElement('p');
+    author.textContent = book.author;
+    author.classList.add('bookCardElement');
+    const pages = document.createElement('p');
+    pages.textContent = book.pages;
+    pages.classList.add('bookCardElement');
+    const read = document.createElement('p');
+    read.textContent = book.read;
+    read.classList.add('bookCardElement');
+
+    libraryCard.appendChild(name);
+    libraryCard.appendChild(author);
+    libraryCard.appendChild(pages);
+    libraryCard.appendChild(read);
+
+    library.appendChild(libraryCard)
+  })
+}
+
+//Clears library node before forEach is executed on array
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
+
